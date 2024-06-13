9241c96a509d117b58e910d9f3924050f4d4e359
thelucassampaio <lucassamp98@gmail.com>
2024-06-13 17:54:57 -0300

    Add key to loading map

--- components/products/ProductsListLoading.tsx
+++ components/products/ProductsListLoading.tsx
@@ -4,7 +4,7 @@ export default function ProductsListLoading() {
   return (
     <div className="container">
       {[...Array(8)].map((_, index) => (
-        <div className="card skeleton" />
+        <div className="card skeleton" key={index} />
       ))}
     </div>
   );
