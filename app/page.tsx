9c096fc4a1829b95d76b151bc0aa43e1c339320e
thelucassampaio <lucassamp98@gmail.com>
2024-06-13 11:14:48 -0300

    Initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["productData"],
+    queryFn: () =>
+      fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/products?page=1&rows=5&sortBy=id&orderBy=DESC`
+      ).then((res) => res.json()),
+  });
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  console.log(data);
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -17,8 +34,8 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/next.svg"
+              alt="Next Logo"
               className={styles.vercelLogo}
               width={100}
               height={24}
